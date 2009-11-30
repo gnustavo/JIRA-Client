8b284e6fa14a06cf6ae6525c26c18a06dc9aa6bf
gnustavo <gnustavo@9ee6d77e-1d8b-11de-bf1e-7b5f7763e386>
2009-11-30 15:49:44 +0000

    bumpping to version 0.22
    git-svn-id: https://jira-client.googlecode.com/svn/trunk@78 9ee6d77e-1d8b-11de-bf1e-7b5f7763e386

--- lib/JIRA/Client.pm
+++ lib/JIRA/Client.pm
@@ -11,11 +11,11 @@ JIRA::Client - An extended interface to JIRA's SOAP API.
 
 =head1 VERSION
 
-Version 0.21
+Version 0.22
 
 =cut
 
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 =head1 SYNOPSIS
 
