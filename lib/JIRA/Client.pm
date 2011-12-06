b842e95124edc61ef67287fbcf281d81362082c8
Gustavo Chaves <gustavo@gnustavo.com>
2011-12-05 23:15:54 -0200

    Bump to version 0.32

--- lib/JIRA/Client.pm
+++ lib/JIRA/Client.pm
@@ -11,11 +11,11 @@ JIRA::Client - An extended interface to JIRA's SOAP API.
 
 =head1 VERSION
 
-Version 0.31
+Version 0.32
 
 =cut
 
-our $VERSION = '0.31';
+our $VERSION = '0.32';
 
 =head1 SYNOPSIS
 
